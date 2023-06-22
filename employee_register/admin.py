abf1dad195e2d83e0bfa706e311f98edcf8a178f
Admin <admin@admins-mbp.lan>
2023-06-22 16:18:55 -0400

    admin

--- employee_register/admin.py
+++ employee_register/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Position
+admin.site.register(Position)
+
+from .models import Employee
+admin.site.register(Employee)
